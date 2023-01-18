6bb78e159198558a2008d5b8d33955ad28884cae
Kadir Gökhan Sezer <74508086+kadirgokhann@users.noreply.github.com>
2023-01-18 20:57:37 +0300

    Update cmpe478_hw2_b.cpp

--- thrust/cmpe478_hw2_b.cpp
+++ thrust/cmpe478_hw2_b.cpp
@@ -52,8 +52,8 @@ unordered_map<int, vector<int>> all_edges_new;
 #define CSR_PRINT 0
 #define NODE_NUM 1850065
 #define EDGE_NUM 16741171
-#define CHECK(i) (i+= (i==6128 || i==84900 || i==84989) ? 20 :0)
-//6128 6146 84989 
+#define CHECK(i) (i+= (i==6128 || i==84900 || i==84989 || i==99612 || i==101731 || i==101755 || i==101797 || i==111555 || i==114273) ? 20 :0)
+//6128 6146 84989 99612
 class CSR{
     public:
         vector<int> row_begin;
@@ -122,6 +122,7 @@ int main(int arg, char* argv[])
     //READIN FILE ENDS
     printf("--> File is read\n");
     int n_number_of_nodes = dictionary.size(); // Number of nodes
+    int NUM_NODES = n_number_of_nodes/16; // Number of edges
     #if DEBUG_FILE_READ
      printf("--> Dictionary: \n");
     {    
@@ -155,6 +156,7 @@ int main(int arg, char* argv[])
     csr->row_begin[0] = 0;
     int priorLen = 0;
     int priorNonzero = 0;
+
     for(int i = 0; i < n_number_of_nodes; i++){
         if(all_edges_new.find(i) == all_edges_new.end()){
             csr->row_begin[i] = 0; 
@@ -215,7 +217,7 @@ int main(int arg, char* argv[])
     thrust::device_vector<double> r0(n_number_of_nodes,1/n_number_of_nodes); // This holds the initial rank of the nodes
     //vector<double> r_next(n_number_of_nodes, 0.0); // This holds the next iteration of the pagerank
     thrust::device_vector<double> r_next(n_number_of_nodes, 0.0); // This holds the next iteration of the pagerank
-
+   
     int iterations = 0; // This holds the number of iterations
     double alpha = 0.20; // Alpha value
     //double diff = 0.0; // Difference between the r0 and r_next
@@ -234,17 +236,17 @@ int main(int arg, char* argv[])
     while(true)
     {
         iterations++;
-        for (int i = 1; i < 99600; i++)
+        for (int i = 1; i < NUM_NODES; i++)
         {
-    
+            CHECK(i);
             // make_permutation_iterator(
             //     InputIterator first,
             //     Iterator map
             // )
             //  Returns an iterator that dereferences to first[map[i]] for each i.
             //  The iterator first must be at least as long as the longest sequence of indices accessed by the iterator map
-            printf("row_begin[%d]: %d\n",i,csr->row_begin[i]);CHECK(i);
-
+            //printf("row_begin[%d]: %d\n",i,csr->row_begin[i]); 
+        
             auto p_iter = thrust::make_permutation_iterator(r0.begin(), csr->col_indices.begin() + csr->row_begin[i]);
             //inner_product(clear
             //    InputIterator1 first1,
@@ -265,20 +267,13 @@ int main(int arg, char* argv[])
         // init	The result is initialized to this value.
         // binary_op	The reduction operation.
          //diff += abs(r_next[i] - r0[i]);
+        //double result = thrust::transform_reduce(r_next.begin(), r_next.end(), r0.begin(), diff.begin(), abs_<double>(), 0.0, thrust::plus<double>());
         thrust::transform(r_next.begin(), r_next.end(), r0.begin(), diff.begin(), abs_());
         double result = thrust::reduce(diff.begin(), diff.end(), 0.0, thrust::plus<double>());     
-    
         // difference_D[i] = | r2_D[i] - r1_D[i] |
-        
+        if (result <= epsilon) break;
+        thrust::copy(r_next.begin(), r_next.end(), r0.begin());
         printf("Iteration: %d, Diff: %.*e\n", iterations,10, diff);
-
-        if (result <= epsilon) 
-            break;
-         else {
-            //r0 = r_next; device to variable
-            thrust::copy(r_next.begin(), r_next.end(), r0.begin());
-        }
-
     }
     myfile.close();
     double et = omp_get_wtime();
